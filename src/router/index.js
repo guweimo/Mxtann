5675be5054e0e613e1b1262b30407adb605df94a
guweimo <lyfw244885@hotmail.com>
2018-05-29 10:15:48 +0800

    delete dist

--- src/router/index.js
+++ src/router/index.js
@@ -8,11 +8,11 @@ import Test from '@/components/test'
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
-  routes: [
-    { path: '/', name: 'Blog', component: Blog },
-    { path: '/index', name: 'HelloWorld', component: HelloWorld },
-    { path: '/save', name: 'Save', component: Save },
-    { path: '/test', name: 'Test', component: Test }
-  ]
+    mode: 'history',
+    routes: [
+        { path: '/', name: 'Blog', component: Blog },
+        { path: '/index', name: 'HelloWorld', component: HelloWorld },
+        { path: '/save', name: 'Save', component: Save },
+        { path: '/test', name: 'Test', component: Test }
+    ]
 })
