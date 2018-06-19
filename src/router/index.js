081f792d3e0af1844a2fdd0b6f7ec0ed61d491dc
guweimo <lyfw244885@hotmail.com>
2018-06-19 16:11:07 +0800

    remove not exist component.

--- src/router/index.js
+++ src/router/index.js
@@ -10,9 +10,7 @@ export default new Router({
     mode: 'history',
     routes: [
         { path: '/', name: 'Blog', component: Blog },
-        { path: '/index', name: 'HelloWorld', component: HelloWorld },
         { path: '/save', name: 'Save', component: Save },
-        { path: '/test', name: 'Test', component: Test },
         { path: '/markPreview', name: 'markPreview', component: markPreview },
     ]
 })
