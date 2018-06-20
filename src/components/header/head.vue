854de3740ff5b2af6d728444549bc92130c24c11
guweimo <lyfw244885@hotmail.com>
2018-06-20 16:51:02 +0800

    create head.vue

---
+++ src/components/header/head.vue
@@ -0,0 +1,20 @@
+<template>
+    <header class="header">
+        <nav>
+            <ul>
+                <li>
+                    <a href="">JavaScript</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "head",
+    data() {
+        return {};
+    }
+};
+</script>
--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,9 +1,17 @@
 <template>
-
+    <head></head>
 </template>
 
 <script>
+import head from '@/compoents/header/head'
+
 export default {
+    components: {
+        head
+    },
+    data() {
+        return {}
+    }
 }
 </script>
 
