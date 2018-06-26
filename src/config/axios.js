51549781e9768dc74d65f39e94d495db50c9ec2e
guweimo <lyfw244885@hotmail.com>
2018-06-26 17:33:18 +0800

    update

---
+++ src/config/axios.js
@@ -0,0 +1,27 @@
+import axios from 'axios'
+
+export const request = {
+    get(url, data, type) {
+        let config = {
+            method: 'GET',
+            url: url,
+            params: data || {},
+            headers: type || {
+                'X-Requested-With': 'XMLHttpRequest',
+            }
+        }
+        return axios(config)
+    },
+    post(url, data, type) {
+        let config = {
+            method: 'POST',
+            url: url,
+            data: data || {},
+            headers: type || {
+                'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+				'X-Requested-With': 'XMLHttpRequest',
+            }
+        }
+        return axios(config);
+    }
+}
