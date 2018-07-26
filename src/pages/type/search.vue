b835a277d3fe06411e92a7969840fe74a27304b7
guweimo <lyfw244885@hotmail.com>
2018-07-26 16:02:05 +0800

    create

---
+++ src/pages/type/search.vue
@@ -0,0 +1,23 @@
+<template>
+    <main>
+        <article-list :articleData="list"></article-list>
+    </main>
+</template>
+
+<script>
+import articleList from '@/components/articleList'
+export default {
+    components: {
+        articleList
+    },
+    data() {
+        return {
+            list: []
+        }
+    },
+    mounted() {
+        
+    }
+}
+</script>
+
