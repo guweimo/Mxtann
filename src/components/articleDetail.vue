d52ca3eaa5bde44250557178dcd9d12dc5161ae4
guweimo <lyfw244885@hotmail.com>
2018-07-31 15:00:25 +0800

    update

--- src/components/articleDetail.vue
+++ src/components/articleDetail.vue
@@ -1,8 +1,16 @@
 <template>
-    <div class="aritcle-detail"></div>
+    <div class="aritcle-detail">
+        <div class="content">
+            <div class="markdown-body">
+
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
+import 'mavon-editor/dist/css/index.css'
+
 export default {
     data() {
         return {}
