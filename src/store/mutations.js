4eddc8997c6a1614057cc26b1c3dd3b266a88cf5
guweimo <lyfw244885@hotmail.com>
2018-08-13 13:28:39 +0800

    把navlist存储到localStorage中。

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -5,6 +5,9 @@ import {
     SELECT_TYPE
 } from './mutation-types'
 import {request} from '@/config/axios'
+import { setStore, getStore } from '@/config/unit'
+
+let navName = 'navlist'
 
 export default {
     [TRUE_BLOG_ROUTER] (state) {
@@ -14,9 +17,14 @@ export default {
         state.isBlogRouter = false
     },
     [GET_NAV_DATA] (state) {
+        let navlist = getStore(navName, true)
+        if (navlist != null) {
+            state.navData = navlist
+        }
         if (state.navData.length == 0) {
             request.get('/apis/home/navlist').then(res => {
                 if (res.data.status == 2000) {
+                    setStore(navName, res.data.data)
                     state.navData = res.data.data
                 }
             })
