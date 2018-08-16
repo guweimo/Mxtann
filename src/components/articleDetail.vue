248bf777055480f11b95059b409266741cae1231
guweimo <lyfw244885@hotmail.com>
2018-08-16 16:37:19 +0800

    update

--- src/components/articleDetail.vue
+++ src/components/articleDetail.vue
@@ -3,6 +3,15 @@
         <div class="title">
             <h1 v-text="detailData.title"></h1>
         </div>
+        <div class="info">
+            <div class="info-submitted">
+                作者：<span v-text="detailData.author"></span>
+                日期：<span v-text="detailData.dateTime"></span>
+            </div>
+            <div class="info-tags">
+
+            </div>
+        </div>
         <div class="content">
             <div class="markdown-body" v-html="detailData.content">
 
@@ -38,6 +47,13 @@ export default {
     }
     .content {
         margin: 20px 0;
+    }
+    .info {
+        color: #ccc;
+        border-bottom: 1px dashed #ccc;
+        padding-bottom: 5px;
+        font-size: 14px;
+        display: felx;
     }
 }
 </style>
