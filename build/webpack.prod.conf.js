d03f8349e62a2d3f6526bbada4f695245079eb86
guweimo <lyfw244885@hotmail.com>
2018-08-20 14:51:31 +0800

    update

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -67,6 +67,7 @@ const webpackConfig = merge(baseWebpackConfig, {
         ? 'index.html'
         : config.build.index,
       template: 'index.html',
+      favicon: path.resolve('./src/assets/picture.jpg'),
       inject: true,
       minify: {
         removeComments: true,
