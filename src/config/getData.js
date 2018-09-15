dfd7ce94a314bceab0002925d59edff149666acd
guweimo <lyfw244885@hotmail.com>
2018-09-15 22:12:52 +0800

    update

--- src/config/getData.js
+++ src/config/getData.js
@@ -1,9 +1,6 @@
 import {request} from './axios'
 import { getStore } from './unit'
 
-// test
-export const getList = () => request.post('/apis/home/list')
-
 // 获取当前用户信息
 export const getUser = () => request.get('/apis/home/getUserInfo', { 
     id:  getStore('userinfo')
