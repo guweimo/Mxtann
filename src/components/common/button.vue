c51f899afe47c50f339185dc4d812b67fb318e4e
guweimo <lyfw244885@hotmail.com>
2018-09-23 10:55:12 +0800

    update

--- src/components/common/button.vue
+++ src/components/common/button.vue
@@ -14,6 +14,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$default-color: #52bab3;
+
 .btn {
     border: 1px solid transparent;
     border-radius: 4px;
@@ -21,6 +23,20 @@ export default {
     outline: auto;
     text-transform: uppercase;
     color: #fff;
+    text-align: center;
+    vertical-align: middle;
+    font-weight: 400;
+    display: inline-block;
+    white-space: nowrap;
+    user-select: none;
+    transform: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
+        border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+}
+
+.btn-default {
+    color: #fff;
+    border-color: $default-color;
+    background-color: $default-color;
 }
 </style>
 
