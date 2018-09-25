5b48358c405197a3816f317f62261c4a56b1d042
guweimo <lyfw244885@hotmail.com>
2018-09-25 20:26:07 +0800

    添加radio公共组件。

---
+++ src/components/common/mtRadio.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="radio-group">
+        <div class="radio-item">
+            <div class="radio-left-item">
+                <span class="blue-radio"></span>
+            </div>
+            <div class="radio-right-item">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
