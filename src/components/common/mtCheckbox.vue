3f4a082fbd770bc85ade7ec3c6ea2b76e4205d13
guweimo <lyfw244885@hotmail.com>
2018-09-26 21:21:38 +0800

    update

---
+++ src/components/common/mtCheckbox.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="checbox-group">
+        <div class="checbox-item">
+            <div class="checbox-left-item">
+                <span class="blue-checbox"></span>
+            </div>
+            <div class="checbox-right-item">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
