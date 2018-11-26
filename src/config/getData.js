3b32514cb36feee8480fa21bfe575b02a84590d8
guweimo <lyfw244885@hotmail.com>
2018-11-26 10:28:59 +0800

    处理获取用户信息的bug

--- src/config/getData.js
+++ src/config/getData.js
@@ -3,5 +3,5 @@ import { getStore } from './unit'
 
 // 获取当前用户信息
 export const getUser = () => request.get('/apis/home/getUserInfo', { 
-    id:  getStore('userinfo')
+    id:  getStore('userinfo', true).id
 })
