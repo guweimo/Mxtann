9da6b0ca5d6b37ae1752fb62921a32e0e236a835
guweimo <lyfw244885@hotmail.com>
2018-11-30 20:30:31 +0800

    create dropdown component

---
+++ src/components/common/dropdown.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="dropdown">
+
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {}
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+    
+</style>
