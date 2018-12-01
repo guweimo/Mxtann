2d82205eda0ebbf7759abaee36ceac9084ee8b4b
guweimo <lyfw244885@hotmail.com>
2018-12-01 19:12:48 +0800

    update

--- src/components/common/dropdown.vue
+++ src/components/common/dropdown.vue
@@ -1,13 +1,19 @@
 <template>
     <div class="dropdown">
-
+        <span></span>
+        <slot></slot>
+        <ul>
+            <li v-for="item in list" :key="item.id"></li>
+        </ul>
     </div>
 </template>
 
 <script>
 export default {
     data() {
-        return {}
+        return {
+            list: []
+        }
     },
 }
 </script>
