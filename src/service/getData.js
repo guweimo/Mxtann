a458fa7f1efef3350d7bbd3459b36e6e579d0481
guweimo <lyfw244885@hotmail.com>
2018-12-11 20:11:21 +0800

    update

--- src/service/getData.js
+++
@@ -1,7 +0,0 @@
-import {request} from '@/config/axios'
-
-export const navType = () => request.get('/apis/home/navlist')
-
-export const pageList = (type = '') => request.post('/apis/home/list', {
-    type
-})
