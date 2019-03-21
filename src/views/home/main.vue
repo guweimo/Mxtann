da2b5057a082018322ce3f7eb175c83ca56cb205
guweimo <lyfw244885@hotmail.com>
2019-03-21 21:50:41 +0800

    重新设置结构

---
+++ src/views/home/main.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="main-page">
+    <head-nav :nav-list="navData"></head-nav>
+    <router-view/>
+    <footer-col></footer-col>
+  </div>
+</template>
+
+<script>
+import headNav from '@/components/header/headNav'
+import footerCol from '@/components/footer/footerCol'
+import { mapState, mapMutations, mapActions } from 'vuex'
+
+export default {
+  data() {
+    return {}
+  },
+  components: {
+    headNav,
+    footerCol
+  },
+  created() {
+    this.getUserInfo()
+  },
+  computed: {
+    ...mapState(['navData'])
+  },
+  mounted() {
+    this.GET_NAV_DATA()
+  },
+  methods: {
+    ...mapMutations(['GET_NAV_DATA']),
+    ...mapActions(['getUserInfo'])
+  }
+}
+</script>
---
+++ src/views/list/cssList.vue
@@ -0,0 +1,28 @@
+<template>
+  <main>
+    <article-list :articleData="list"></article-list>
+  </main>
+</template>
+
+<script>
+import articleList from '@/components/articleList'
+import { getList } from '@/apis/article'
+
+export default {
+  components: {
+    articleList
+  },
+  data() {
+    return {
+      list: {}
+    }
+  },
+  mounted() {
+    getList('2').then(res => {
+      if (res.data.status == 2000) {
+        this.list = res.data.data
+      }
+    })
+  }
+}
+</script>
