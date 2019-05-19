1e9b74f402522bc66ff7b932a4bb8ba6eb2cbc82
guweimo <lyfw244885@hotmail.com>
2019-05-19 18:55:03 +0800

    delete vue-cli2 file

--- src/components/message/index.js
+++
@@ -1,7 +0,0 @@
-import messageBox from './src/index'
-
-export default {
-  install(Vue) {
-    Vue.prototype.$message = messageBox
-  }
-}
--- src/store/mutation-types.js
+++
@@ -1,7 +0,0 @@
-export const USER = {
-  GET_NAV_DATA: 'getNavData',
-  SELECT_TYPE: 'selectType',
-  GET_USERINFO: 'getUserinfo',
-  REMOVE_USERINFO: 'removeUserinfo',
-  UPDATE_SEARCH_TITLE: 'updateSearchTitle',
-}
